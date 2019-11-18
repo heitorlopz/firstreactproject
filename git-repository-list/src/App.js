e46b14b993e7d2080ff6c13a6ed9784da5e441ea
heitorlopz <fheitorlopes@gmail.com>
2019-11-18 16:10:45 -0300

    setting main page styles

--- git-repository-list/src/App.js
+++ git-repository-list/src/App.js
@@ -2,9 +2,16 @@ import React from 'react';
 
 import Routes from './routes';
 
+import GlobalStyle from './styles/global';
+
 // app -> componente raiz da aplicação
 function App() {
-  return <Routes />;
+  return (
+    <>
+      <Routes />
+      <GlobalStyle />
+    </>
+  );
 }
 
 export default App;
