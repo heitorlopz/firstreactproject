0c27a45c8f7c4aac99dda54e5cb510f8fd9b8cf9
heitorlopz <fheitorlopes@gmail.com>
2019-11-20 13:49:35 -0300

    defining proptypes and loading api data

--- git-repository-list/src/pages/Repository/index.js
+++ git-repository-list/src/pages/Repository/index.js
@@ -1,7 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import api from '../../services/api';
 
 // import { Container } from './styles';
 
-export default function Repository() {
-  return <h1>Repository</h1>;
+export default class Repository extends Component {
+  static propTypes = {
+    match: PropTypes.shape({
+      params: PropTypes.shape({
+        repository: PropTypes.string,
+      }),
+    }).isRequired,
+  };
+
+  // como é um unico repositorio, inicia como objeto
+  state = {
+    repository: {},
+    issues: [],
+    loading: true,
+  };
+
+  // Assíncrono pq vai fazer chamadas na API
+  async componentDidMount() {
+    const { match } = this.props;
+
+    const repoName = decodeURIComponent(match.params.repository);
+
+    // utilizamos dessa forma para as duas rotas executarem ao mesmo tempo
+    const [repository, issues] = await Promise.all([
+      await api.get(`/repos/${repoName}`),
+      await api.get(`/repos/${repoName}/issues`, {
+        params: {
+          state: 'open',
+          per_page: 5,
+        },
+      }),
+    ]);
+
+    this.setState({
+      repository: repository.data,
+      issues: issues.data,
+      loading: false,
+    });
+  }
+
+  render() {
+    const { repository, issues, loading } = this.state;
+
+    return <h1>Repository</h1>;
+  }
 }
